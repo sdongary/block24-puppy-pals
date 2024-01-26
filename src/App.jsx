3b633f9ed2330710841c3753d28a6f8024e2f118
Sachin Dongary <siddharth.sachin89@gmail.com>
2024-01-25 20:06:55 -0600

    final

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
+
+
+function App() {
+  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  
+  //console.log("puppyList: ", puppies);
+
+  function handleClick(id){
+    setFeatPupId(id);
+    console.log(featPupId);
+    }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId); 
+  console.log(featPupId)
+
+  return (
+    <div className='App'>
+      { 
+        puppies.map((puppy) => {
+          return (
+          <p onClick={() => 
+            setFeatPupId(puppy.id)} 
+            key={puppy.id}>{puppy.name}
+          </p>
+          );
+        })
+      }
+
+{featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+    </div>
+  )
+}
+
+export default App
